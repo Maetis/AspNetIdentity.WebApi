3b324a6939fc0ddaac4f39890e56aee1dba56bb0
Kevin L <kevin.lecouvey@gmail.com>
2015-12-18 22:59:17 -0500

    Remove log option

--- Infrastructure/install-Framework.Net_45.ps1
+++ Infrastructure/install-Framework.Net_45.ps1
@@ -2,5 +2,5 @@ $msi_file_name = "dotNetFx45_Full_setup.exe"
 $download_url = "https://download.microsoft.com/download/B/A/4/BA4A7E71-2906-4B2D-A0E1-80CF16844F5F/$msi_file_name"
 
 (New-Object System.Net.WebClient).DownloadFile($download_url, "C:\Windows\Temp\$msi_file_name")
-&"C:\Windows\Temp\$msi_file_name" /passive /norestart /log .
+&"C:\Windows\Temp\$msi_file_name" /passive /norestart
 
