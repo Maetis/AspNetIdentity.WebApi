e976b5940c866f217b07c09d04c46f018579b952
Kevin L <kevin.lecouvey@gmail.com>
2015-12-20 19:39:43 -0500

    Fix script for WebApp deployment

--- Infrastructure/deploy-app.ps1
+++ Infrastructure/deploy-app.ps1
@@ -1,3 +1,9 @@
+$strAppConfigFile = "$env:USERPROFILE\Documents\IISExpress\config\applicationhost.config"
+
+If (-Not Test-Path $strAppConfigFile) {
+	Start-Process "$env:ProgramFiles\IIS Express\iisexpress.exe" -workingdirectory "$env:ProgramFiles\IIS Express" -WindowStyle Hidden -ErrorAction SilentlyContinue
+}
+
 &"$env:ProgramFiles\IIS Express\appcmd.exe" add site /name:PackerSite /bindings:"http/*:12345:localhost" /physicalPath:"$env:USERPROFILE\Temp"
 
 
