19bb462cf8527142721d63810c7ec0c3783ae7b1
Kevin L <kevin.lecouvey@gmail.com>
2015-12-21 17:33:53 -0500

    Wrong physical path for IIS Express site

--- Infrastructure/deploy-app.ps1
+++ Infrastructure/deploy-app.ps1
@@ -4,6 +4,6 @@ If (-Not(Test-Path $strAppConfigFile)) {
 	Start-Process "$env:ProgramFiles\IIS Express\iisexpress.exe" -workingdirectory "$env:ProgramFiles\IIS Express" -WindowStyle Hidden -ErrorAction SilentlyContinue
 }
 
-&"$env:ProgramFiles\IIS Express\appcmd.exe" add site /name:PackerSite /bindings:"http/*:12345:localhost" /physicalPath:"$env:USERPROFILE\Temp"
+&"$env:ProgramFiles\IIS Express\appcmd.exe" add site /name:PackerSite /bindings:"http/*:12345:localhost" /physicalPath:"$env:USERPROFILE\tmp"
 
 
