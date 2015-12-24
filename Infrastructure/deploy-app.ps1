fe11aa1b1e41b4fab3a5c585ada3d92aa0c9608c
Kevin L <kevin.lecouvey@gmail.com>
2015-12-23 22:11:34 -0500

    tweak scripts for next CD step

--- Infrastructure/deploy-app.ps1
+++ Infrastructure/deploy-app.ps1
@@ -4,6 +4,10 @@ If (-Not(Test-Path $strAppConfigFile)) {
 	Start-Process "$env:ProgramFiles\IIS Express\iisexpress.exe" -workingdirectory "$env:ProgramFiles\IIS Express" -WindowStyle Hidden -ErrorAction SilentlyContinue
 }
 
-&"$env:ProgramFiles\IIS Express\appcmd.exe" add site /name:PackerSite /bindings:"http/*:12345:localhost" /physicalPath:"$env:USERPROFILE\tmp"
+&"$env:ProgramFiles\IIS Express\appcmd.exe" add site /name:PackerSite /bindings:"http/*:12345:*" /physicalPath:"$env:USERPROFILE\tmp"
+
+netsh.exe advfirewall firewall add rule name="Open Port 12345" dir=in action=allow protocol=TCP localport=12345
+netsh http add urlacl url=http://*:12345/ user="Tout le monde"
+
 
 
